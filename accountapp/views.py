5d9085942a0ad73bb62f3511d348849d37e734a9
Jun <misojun50@gmail.com>
2021-07-05 11:41:57 +0900

    7/5 mytest2 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_again(request):
-    return render(request,'base.html')
+    return render(request,'accountapp/hello_again.html')
