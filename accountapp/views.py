7a38c8b3784d19fb18edfdb164ce20bf9617e229
Jun <misojun50@gmail.com>
2021-07-12 11:39:00 +0900

    7/12 mytest2 commit2

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,15 +2,20 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from accountapp.models import HelloAgain
+
 
 def hello_again(request):
     if request.method == "POST":
 
         temp = request.POST.get('input_text')
 
+        hel_again = HelloAgain()
+        hel_again.text = temp
+        hel_again.save()
 
         return render(request,'accountapp/hello_again.html',
-                      context={'text': temp })
+                      context={'hel_again': hel_again })
     else:
         return render(request, 'accountapp/hello_again.html',
                       context={'text': 'GET METHOD!'})
