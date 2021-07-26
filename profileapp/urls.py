c8354b9cfc1cd7dbf346e61c7f284c641098dbf4
Jun <misojun50@gmail.com>
2021-07-26 12:25:37 +0900

    7/26 mytest2 commit2

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,4 +1,10 @@
+from django.urls import path
+
+from profileapp.views import ProfileCreationView
+
+app_name = 'profileapp'
+
 urlpatterns = [
 
-
+    path('create/', ProfileCreationView.as_view(), name='create')
 ]
