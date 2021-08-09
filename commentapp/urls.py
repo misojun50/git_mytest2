24b1a640594f7598bb5e14a08d110cbe783e8207
Jun <misojun50@gmail.com>
2021-08-09 11:47:44 +0900

    8/9 mytest2 commit2

--- commentapp/urls.py
+++ commentapp/urls.py
@@ -1,4 +1,11 @@
+from django.urls import path
+
+from commentapp.views import CommentCreationView
+
+app_name = 'commentapp'
 
 urlpatterns = [
+
+    path('create/', CommentCreationView.as_view(), name='create'),
 
 ]
--- commentapp/views.py
+++ commentapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from commentapp.forms import CommentCreationForm
+from commentapp.models import Comment
+
+
+class CommentCreationView(CreateView):
+    model = Comment
+    form_class = CommentCreationForm
+    template_name = 'commentapp/create.html'
+
+    def get_success_url(self):
+        return reverse_lazy('articleapp:detail', kwargs={'pk':self.object.article.pk})
