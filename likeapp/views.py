1e1b3a1edfdc72e5277898f53557dfac7dbe1ac9
Jun <misojun50@gmail.com>
2021-08-24 17:24:19 +0900

    8/24 mytest2 commit3

--- likeapp/views.py
+++ likeapp/views.py
@@ -1,3 +1,32 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+from django.views.generic import RedirectView
+
+from articleapp.models import Article
+from likeapp.models import LikeRecord
+
+
+class LikeArticleView(RedirectView):
+
+    def get(self, request, *args, **kwargs):
+
+        user = request.user
+        article = Article.objects.get(pk=kwargs['article_pk'])
+
+        like_record = LikeRecord.objects.filter(user=user, article=article)
+
+        if like_record.exists():
+            return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk':kwargs['article_pk']}))
+        else:
+            LikeRecord(user=user, article=article).save()
+
+        article.like += 1
+        article.save()
+
+        return super().get(request, *args, **kwargs)
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('articleapp:detail', kwargs={'pk':kwargs['article_pk']})
