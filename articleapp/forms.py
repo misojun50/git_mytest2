9fe129ff7e55b65a526b340e497d4404e9c904a5
Jun <misojun50@gmail.com>
2021-08-26 12:26:19 +0900

    8/26 mytest2 commit3

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -5,7 +5,7 @@ from articleapp.models import Article
 
 
 class ArticleCreationForm(ModelForm):
-    content = forms.CharField(widget=forms.Textarea(attrs={"class":"editable", "style":"text-align: left; min-height 10rem;"}))
+    content = forms.CharField(widget=forms.Textarea(attrs={"class":"editable", "style":"text-align: left; min-height: 10rem;"}))
 
     class Meta:
         model = Article
